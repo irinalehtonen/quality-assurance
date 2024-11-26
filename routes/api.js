e2f993cf9ad01394eb48f4108184ed5aba680365
irinalehtonen <130436369+irinalehtonen@users.noreply.github.com>
2024-11-26 10:53:34 +0200

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,37 @@
-'use strict';
+// routes/api.js
 
-const expect = require('chai').expect;
-const ConvertHandler = require('../controllers/convertHandler.js');
+const express = require('express');
+const ConvertHandler = require('../controllers/convertHandler');
+const router = express.Router();
+const convertHandler = new ConvertHandler();
 
-module.exports = function (app) {
-  
-  let convertHandler = new ConvertHandler();
+router.get('/api/convert', function(req, res) {
+  const input = req.query.input;
 
-};
+  if (!input) {
+    return res.json({ error: 'No input provided' });
+  }
+
+  const num = convertHandler.getNum(input);
+  const unit = convertHandler.getUnit(input);
+
+  if (!unit) {
+    return res.json({ error: 'Invalid unit' });
+  }
+
+  const returnUnit = convertHandler.getReturnUnit(unit);
+  const convertedNum = convertHandler.convert(num, unit);
+  const spelledOutUnit = convertHandler.spellOutUnit(unit);
+  const returnSpelledOutUnit = convertHandler.spellOutUnit(returnUnit);
+
+  res.json({
+    initNum: num,
+    initUnit: unit,
+    returnNum: convertedNum,
+    returnUnit: returnUnit,
+    initUnitSpelled: spelledOutUnit,
+    returnUnitSpelled: returnSpelledOutUnit,
+  });
+});
+
+module.exports = router;
